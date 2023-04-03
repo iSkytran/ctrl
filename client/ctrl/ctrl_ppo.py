cc3ecd109d69029a6957008c05355b37b0a726c0
Ethan Tran <rizcielbleu@gmail.com>
2023-04-03 16:44:33 -0400

    Add logging and checkpoints

--- client/ctrl/ctrl_ppo.py
+++ client/ctrl/ctrl_ppo.py
@@ -1,11 +1,25 @@
-import gymnasium as gym
-from ctrl import CtrlEnv
+import os
 from stable_baselines3 import PPO
-from stable_baselines3.common.env_checker import check_env
+from stable_baselines3.common.callbacks import CheckpointCallback
 from stable_baselines3.common.env_util import make_vec_env
+from stable_baselines3.common.logger import configure
 from stable_baselines3.common.vec_env import VecFrameStack
+
+output_path = os.getenv("CTRL_OUTPUT_PATH", "./rl_out")
+checkpoint_frequency = os.getenv("CTRL_CHECKPOINT_FREQUENCY", 100000)
+timesteps = os.getenv("CTRL_TIMESTEPS", 20000000)
+
+logger = configure(output_path, ["stdout", "log", "csv"])
+checkpoint_callback = CheckpointCallback(
+    save_freq=checkpoint_frequency,
+    save_path=output_path,
+    name_prefix="rl_model",
+    save_replay_buffer=True,
+    save_vecnormalize=True,
+)
 
 env = make_vec_env("ctrl", n_envs=1)
 env = VecFrameStack(env, n_stack=4)
 model = PPO("CnnPolicy", env)
-model.learn(total_timesteps=1000000)
+model.learn(total_timesteps=timesteps, callback=checkpoint_callback)
+model.save(output_path)
