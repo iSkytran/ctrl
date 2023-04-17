41cb007f591bfe9aac68585dca77779edd534467
Ethan Tran <rizcielbleu@gmail.com>
2023-04-17 17:31:13 -0400

    Add dependencies

--- client/setup.py
+++ client/setup.py
@@ -3,5 +3,5 @@ from setuptools import setup
 setup(
     name="ctrl",
     version="0.0.0",
-    install_requires=["gymnasium", "mss==7.0.1", "pyzmq==25.0.2"]
+    install_requires=["gymnasium", "mss>=7.0.1", "pyzmq>=25.0.2", "scikit-image>=0.20.0"]
 )
